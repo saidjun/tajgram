73508d263d6eb118f6ca990cdd9a8691f3e76da8
saidjun <79911205+saidjun@users.noreply.github.com>
2026-06-09 14:00:04 +0300

    Refactor XML line handling in xml_shield.py

--- .github/workflows/xml_shield.py
+++ .github/workflows/xml_shield.py
@@ -31,5 +31,8 @@ def ultimate_factory_restore():
             continue
             
         # Агар сатр сохтори заводии XML-ро дошта бошад, бетағйир мемонад
-     if '<string name=' in line or '</string>' in line or '<resources>' in line or '</resources>' in line or '
+             if '<string name=' in line or '</string>' in line:
+            fixed_lines.append(line)
+        elif '<resources>' in line or '</resources>' in line:
+            fixed_lines.append(line)
 
